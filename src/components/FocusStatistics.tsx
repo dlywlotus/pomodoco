f64e940cd290d97ca3ac0723fa3620695b5a0b7e
dlywlotus <dylanlowywgit@gmail.com>
2024-07-22 10:34:31 +0800

    bug fix

--- src/components/FocusStatistics.tsx
+++ src/components/FocusStatistics.tsx
@@ -10,10 +10,16 @@ type props = {
 
 export default function FocusStatistics({ user, currentWeek }: props) {
   const today = dayjs().format("ddd").toLocaleLowerCase();
+  const todayDate = dayjs().format("YYYY-MM-DD");
   const todayTimeInMinutes = Number(currentWeek[today as keyof week]);
-
   const prevLogDate = dayjs(user.prev_log);
-  const streak = user.prev_log ? prevLogDate.diff(user.streak_start, "day") + 1 : 0;
+
+  const streak =
+    user.prev_log !== todayDate
+      ? prevLogDate.diff(user.streak_start, "day") + 1
+      : todayTimeInMinutes > 0
+      ? 1
+      : 0;
 
   const toHoursAndMinutes = (timeInMinutes: number) => {
     const hours = Math.trunc(timeInMinutes / 60);
